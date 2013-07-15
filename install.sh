5382a3a58116f06e5c7a48d881c48599a6b262c7
Baptiste Fontaine <batifon@yahoo.fr>
2013-07-15 20:47:59 +0200

    Install script improved.

--- install.sh
+++ install.sh
@@ -1,32 +1,59 @@
 #! /usr/bin/env bash
 
-echo -n "Looking for curl... "
-which curl > /dev/null || echo "not "
-echo "found."
+function __install_rfc() {
 
-echo -n "Looking for less... "
-which less > /dev/null || echo -n "not "
-echo "found."
+printf "Looking for curl... "
+which curl > /dev/null
+if [ "$?" = "0" ]; then
+    echo "found."
+else
+    printf "not found. I'll try with wget... "
+    which wget > /dev/null
+    if [ "$?" -eq 0 ]; then
+        echo "found."
+    else
+        echo 'not found!'
+        return 1
+    fi
+fi
 
-echo -n "Does \$HOME/bin exists?... "
-ls $HOME/bin > /dev/null 2>&1
-if [ $? -ne 0 ]; then
+printf "Looking for less... "
+which less > /dev/null
+if [ "$?" = "0" ]; then
+    echo "found"
+else
+    if [ -n "$PAGER" ]; then
+        echo "not found. I'll use '$PAGER' instead."
+    else
+        echo 'not found!'
+        return 1
+    fi
+fi
+
+printf "Does \$HOME/bin exists?... "
+if [ ! -d "$HOME/bin" ]; then
     echo "no."
     
-    echo -n "Creating \$HOME/bin... "
-    mkdir $HOME/bin && echo "done" || echo "fail!"
-
-    echo -n "Adding it to the \$PATH... "
-    echo "export PATH=\$PATH:\$HOME/bin" >> $HOME/.bashrc
-    echo "done."
-
+    printf "Creating \$HOME/bin... "
+    mkdir $HOME/bin 2>/dev/null && echo "done." || echo 'fail!'
 else
     echo "yes."
 fi
 
-echo -n "Downloading rfc... "
-curl -fs https://raw.github.com/bfontaine/rfc/master/rfc > "$HOME/bin/rfc" \
-    && echo "done." || echo "fail!"
+echo ":$PATH:" | grep -q ":$HOME/bin:"
+if [ "$?" != "0" ]; then
+    printf "Adding it to the \$PATH... "
+    echo "\n# Added by 'rfc' installer\nexport PATH=\$PATH:\$HOME/bin" \
+        >> $HOME/.bash_profile \
+            && echo "done." || echo 'fail!'
+fi
 
-echo -n "Making it executable... "
-chmod u+x "$HOME/bin/rfc" && echo "done." || echo "fail!"
+printf "Downloading rfc... "
+curl -fs https://raw.github.com/bfontaine/rfc/master/rfc > "$HOME/bin/rfc" \
+    && echo "done." || echo 'fail!'
+
+printf "Making it executable... "
+chmod u+x "$HOME/bin/rfc" && echo "done." || echo 'fail!'
+
+}
+__install_rfc;
